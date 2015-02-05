09faeb9e928817d66e8d452df99e67bbea8c26ac
Lance Sanchez <lance.sanchez@gmail.com>
2015-02-05 14:55:29 -0800

    requiring the formatter from json_expanded

--- lib/cucumber/json_expanded.rb
+++ lib/cucumber/json_expanded.rb
@@ -2,6 +2,7 @@ require 'cucumber/formatter/gherkin_formatter_adapter'
 require 'cucumber/formatter/io'
 require 'gherkin/formatter/json_formatter'
 
+require "cucumber/json_expanded/formatter"
 require "cucumber/json_expanded/version"
 
 module Cucumber
